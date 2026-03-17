f2dbde1c3e045401367f3e958eb77390c7ab5c81
Eryvan Barbosa <eryvannssantos@gmail.com>
2026-03-17 19:57:33 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,99 @@
+let botao = document.getElementById("registrar")
+let listaVendas = document.getElementById("lista-vendas")
+let totalElemento = document.getElementById("total")
+
+let totalDia = 0
+let vendas = []
+
+let precos = {
+"Sorvete 10L": 80,
+"Sorvete 5L": 50,
+"Sorvete 1L": 10,
+"Sorvete 200ML": 2,
+"Sundae": 2.5,
+
+"Açaí 10L": 80,
+"Açaí 5L": 50,
+"Açaí 1L": 10,
+
+"Ao leite": 2,
+"Skimo": 3,
+"Paleta Mexicana": 4
+}
+
+botao.addEventListener("click", registrarVenda)
+
+function registrarVenda(){
+
+let produto = document.getElementById("produto").value
+let quantidade = document.getElementById("quantidade").value
+
+if(quantidade == "" || quantidade <= 0){
+alert("Digite uma quantidade válida")
+return
+}
+
+let preco = precos[produto]
+let totalVenda = preco * quantidade
+
+totalDia += totalVenda
+
+let linha = document.createElement("p")
+
+linha.textContent =
+produto + " | " +
+quantidade + " unidades vendidas | R$" +
+totalVenda + " "
+
+let botaoRemover = document.createElement("button")
+botaoRemover.textContent = "❌"
+
+linha.appendChild(botaoRemover)
+
+listaVendas.appendChild(linha)
+
+totalElemento.textContent = "Total do Dia: R$" + totalDia
+
+botaoRemover.addEventListener("click", function(){
+
+linha.remove()
+
+totalDia -= totalVenda
+
+totalElemento.textContent = "Total do Dia: R$" + totalDia
+})
+
+ vendas.push({
+produto: produto,
+quantidade: quantidade,
+total: totalVenda
+})
+
+localStorage.setItem("vendas", JSON.stringify(vendas))
+
+}
+
+let vendasSalvas = localStorage.getItem("vendas")
+
+if(vendasSalvas){
+
+vendas = JSON.parse(vendasSalvas)
+
+vendas.forEach(function(venda){
+
+let linha = document.createElement("p")
+
+linha.textContent =
+venda.produto + " | " +
+venda.quantidade + " unidades vendidas | R$" +
+venda.total
+
+listaVendas.appendChild(linha)
+
+totalDia += venda.total
+
+})
+
+totalElemento.textContent = "Total do Mês: R$" + totalDia
+
+} 
